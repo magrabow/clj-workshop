8172fa977233bebe6847fafe3173a70c011229c3
Mateusz Grabowski <m.grabowski@smartrecruiters.com>
2016-03-24 23:40:21 +0100

    Implement parens validator.

--- src/clj_workshop/parens_validator.clj
+++ src/clj_workshop/parens_validator.clj
@@ -1,8 +1,16 @@
 (ns clj-workshop.parens-validator)
 
+(defn- balancer [letters depth]
+  (cond
+    (< depth 0) false
+    (and (> depth 0) (empty? letters)) false
+    (and (= depth 0) (empty? letters)) true
+    :else (let [i (case (first letters) \( 1 \) -1 0)]
+            (balancer (rest letters) (+ depth i)))))
+
 (defn parens-balanced?
   "Returns true if parens in the provided string
   are balanced, false otherwise. Ignores any other
   characters."
   [s]
-  (throw (ex-info "Unimplemented" {})))
+  (balancer (seq s) 0))
