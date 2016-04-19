8f0f81e1e474bbdf97aeb25492a76034b9141740
Mateusz Grabowski <m.grabowski@smartrecruiters.com>
2016-04-19 16:00:01 +0200

    Add concurrency tasks solution.

--- src/clj_workshop/concurrency_exe.clj
+++ src/clj_workshop/concurrency_exe.clj
@@ -5,11 +5,29 @@
   "Start threads-count threads and make each increment the shared number (with initial value 0) inc-count times.
   Return the value of the shared number"
   [threads-count inc-count]
-  (unimplemented))
+  (let [counter (atom 0)
+        futures (map
+                  (fn [_] (future (dotimes [n inc-count] (swap! counter inc))))
+                  (range threads-count))]
+    (doall futures)
+    (doseq [f futures] @f)
+    @counter))
 
 (defn bank-simulation
   "Start threads-count threads and make each execute txn-count transactions where random amount of money
   is transferred between two accounts.
   Returns the sum of money from both accounts"
   [threads-count txn-count account1-init-val account2-init-val]
-  (unimplemented))
+  (let [acc1-bal (ref account1-init-val)
+        acc2-bal (ref account2-init-val)
+        futures (map (fn [_]
+                       (future
+                         (dotimes [n txn-count]
+                           (let [x (- (rand-int 9999) 5000)]
+                             (dosync
+                               (alter acc1-bal - x)
+                               (alter acc2-bal + x))))))
+                      (range threads-count))]
+    (doall futures)
+    (doseq [f futures] @f)
+    (+ @acc1-bal @acc2-bal)))
--- test/clj_workshop/concurrency_exe_test.clj
+++ test/clj_workshop/concurrency_exe_test.clj
@@ -2,8 +2,8 @@
   (:require [clj-workshop.concurrency-exe :as ut]
             [clojure.test :refer :all]))
 
-#_(deftest multi-increment-test
+(deftest multi-increment-test
   (is (= 4000 (ut/multi-increment 4 1000))))
 
-#_(deftest bank-simulation
+(deftest bank-simulation
   (is (= 10000 (ut/bank-simulation 4 1000 7000 3000))))
